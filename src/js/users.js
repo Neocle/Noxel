12deb23008ebfa5338acd6153f810f3fdf4a8336
Neocle <lilian433700@gmail.com>
2026-06-10 19:09:01 +0200

    added a correctly displayed profile page, also added some statistics methods

--- src/js/users.js
+++ src/js/users.js
@@ -1,114 +1,129 @@
-    let users = [];
-    let currentUser = {};
+let users = [];
+let currentUser = {};
 
-    document.addEventListener("DOMContentLoaded", () => {
-        loadUsers();
+document.addEventListener("DOMContentLoaded", () => {
+    loadUsers();
 
-        const me = localStorage.getItem("me");
-    });
+    const me = localStorage.getItem("me");
+});
 
-    function saveUsers() {
-        localStorage.setItem("users", JSON.stringify(users));
+function saveUsers() {
+    localStorage.setItem("users", JSON.stringify(users));
+}
+
+function loadUsers() {
+    const data = localStorage.getItem("users");
+    const current = localStorage.getItem("me");
+
+    if (data) {
+        users = JSON.parse(data);
+        currentUser = JSON.parse(current);
     }
+}
 
-    function loadUsers() {
-        const data = localStorage.getItem("users");
-        const current = localStorage.getItem("me");
-
-        if (data) {
-            users = JSON.parse(data);
-            currentUser = JSON.parse(current);
+function userExists(email) {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].email === email) {
+            return true;
         }
     }
 
-    function userExists(email) {
-        for (let i = 0; i < users.length; i++) {
-            if (users[i].email === email) {
-                return true;
-            }
-        }
+    return false;
+}
 
+function getUser(email) {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].email === email) {
+            return users[i];
+        }
+    }
+
+    return null;
+}
+
+function saveCurrentUser(user) {
+    if (user) {
+        currentUser = user;
+        localStorage.setItem("me", JSON.stringify(user));
+    }
+}
+
+function getCurrentUser() {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].email === currentUser.email) {
+            return users[i];
+        }
+    }
+
+    return null;
+}
+
+function getFollowers(user) {
+    let count = 0;
+
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].follows.includes(user.email)) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+// user1 va suivre user
+function follow(emailUser1, emailUser2) {
+    if (emailUser1 === emailUser2)
+        return;
+
+    const user1 = getUser(emailUser1);
+    if (user1.follows.includes(emailUser2))
+        return;
+
+    user1.follows.push(emailUser2);
+    currentUser.follows.push(emailUser2);
+
+    saveUsers();
+    saveCurrentUser();
+
+    addPosts();
+}
+
+function unfollow(emailUser1, emailUser2) {
+    if (emailUser1 === emailUser2)
+        return;
+
+    const user1 = getUser(emailUser1);
+    if (!user1.follows.includes(emailUser2))
+        return;
+
+    user1.follows = user1.follows.filter(email => email !== emailUser2);
+
+    saveUsers();
+    saveCurrentUser();
+
+    addPosts();
+}
+
+// retourne true si user1 suit user2
+// currentUser suis author?
+function follows(user1, user2) {
+    if (user1 === user2)
         return false;
-    }
 
-    function getUser(email) {
-        for (let i = 0; i < users.length; i++) {
-            if (users[i].email === email) {
-                return users[i];
-            }
-        }
+    return user1.follows.includes(user2.email);
+}
 
-        return null;
-    }
+function getNbFollowers(user) {
+    let f = 0;
 
-    function saveCurrentUser(user) {
-        if (user) {
-            currentUser = user;
-            localStorage.setItem("me", JSON.stringify(user));
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].email === user.email)
+            continue;
+
+        if (follows(users[i], user)) {
+            f++;
         }
     }
 
-    function getCurrentUser() {
-        for (let i = 0; i < users.length; i++) {
-            if (users[i].email === currentUser.email) {
-                return users[i];
-            }
-        }
-
-        return null;
-    }
-
-    function getFollowers(user) {
-        let count = 0;
-
-        for (let i = 0; i < users.length; i++) {
-            if (users[i].follows.includes(user.email)) {
-                count++;
-            }
-        }
-
-        return count;
-    }
-
-    // user1 va suivre user
-    function follow(emailUser1, emailUser2) {
-        if (emailUser1 === emailUser2)
-            return;
-
-        const user1 = getUser(emailUser1);
-        if (user1.follows.includes(emailUser2))
-            return;
-
-        user1.follows.push(emailUser2);
-        currentUser.follows.push(emailUser2);
-
-        saveUsers();
-        saveCurrentUser();
-
-        addPosts();
-    }
-
-    function unfollow(emailUser1, emailUser2) {
-        if (emailUser1 === emailUser2)
-            return;
-
-        const user1 = getUser(emailUser1);
-        if (!user1.follows.includes(emailUser2))
-            return;
-
-        user1.follows = user1.follows.filter(email => email !== emailUser2);
-
-        saveUsers();
-        saveCurrentUser();
-
-        addPosts();
-    }
-
-    // retourne true si user1 suit user2
-    // currentUser suis author?
-    function follows(user1, user2) {
-        if (user1 === user2)
-            return false;
-
-        return user1.follows.includes(user2.email);
-    }
+    return f;
+}
